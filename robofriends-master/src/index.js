01ba6d2836ac7cc03ad2653e58a0d7301ec61c59
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-08-12 09:06:04 -0700

    true app

--- robofriends-master/src/index.js
+++ robofriends-master/src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+
+
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { requestRobots, searchRobots } from './reducers'
 
+import './index.css'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const logger = createLogger()
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
--- robofriends-pwa-master/robofriends-pwa-master/README.md
+++
@@ -1,9 +0,0 @@
-# robofriends-pwa
-adding PWA capabilities to a react app
-
-To run the project:
-
-1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
-
